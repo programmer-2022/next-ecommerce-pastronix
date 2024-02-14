eb0b54943c4b6103900e7686a70cb65935baed7b
Cesar Martinez <km.music92@gmail.com>
2024-02-14 11:38:36 -0500

    feat: update main pages, add banner image

---
+++ app/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Container } from "./Container";
+export { default as HomeBanner } from "./HomeBanner";
+export { default as Footer } from "./footer/Footer";
+export { default as FooterList } from "./footer/FooterList";
+export { default as NavBar } from "./nav/NavBar";
+export { default as ProductCard } from "./products/ProductCard";
